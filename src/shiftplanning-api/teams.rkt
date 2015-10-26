f7152d8a8603bd9270e5eb6c6d4f8f828e3a3daa
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-27 01:57:47 +0200

    Check for employee_id instead of presence of schedule

--- src/shiftplanning-api/teams.rkt
+++ src/shiftplanning-api/teams.rkt
@@ -1,29 +1,11 @@
 #lang racket/base
 
-(provide team/row)
-(define team/row
-  '((|909929| .  "French CS Agent")
-    (|909928| .  "Polish CS Agent")
-    (|909932| .  "Czech CS Agent")
-    (|909927| .  "ROW TM")
-    (|909930| .  "Swedish CS Agent")))
-
-(provide team/english)
-(define team/english
-  '((|907443| . "English BCP CS Agent")))
+(require "team-members.rkt")
 
 (provide employee-in-team?)
 (define (employee-in-team? employee team)
-  (ormap (lambda (team-schedule)
-           (hash-has-key? (hash-ref employee
-                                    'schedules)
-                          (car team-schedule)))
-         team))
-
-(provide employee-in-team?/on-now)
-(define (employee-in-team?/on-now employee team)
-  (ormap (lambda (team-schedule)
+  (ormap (lambda (team-member)
            (equal? (hash-ref employee
-                             'schedule_name)
-                   (cdr team-schedule)))
+                             'employee_id)
+                   (car team-member)))
          team))
