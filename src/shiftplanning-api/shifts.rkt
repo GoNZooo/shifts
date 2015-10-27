9246307ab1dfbc8610c14a9982ba694ccf859958
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-27 02:19:27 +0200

    Make version of employee-in-team? that still searches schedules

--- src/shiftplanning-api/shifts.rkt
+++ src/shiftplanning-api/shifts.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (require "api-call.rkt"
-         "teams.rkt")
+         "teams.rkt"
+         "team-members.rkt")
 
 (provide get/shifts)
 (define (get/shifts)
@@ -14,10 +15,10 @@
 
 (define (get/on-now/team [team-name team/row])
   (filter (lambda (employee)
-            (employee-in-team?/on-now employee
-                                      team-name))
-          (get-on-now)))
+            (employee-in-team? employee
+                               team-name))
+          (get/on-now)))
 
 (module+ main
   (require racket/pretty)
-  (pretty-print (get/on-now/team team/english)))
+  (pretty-print (get/on-now/team team/italian)))
--- src/shiftplanning-api/teams.rkt
+++ src/shiftplanning-api/teams.rkt
@@ -2,10 +2,20 @@
 
 (require "team-members.rkt")
 
-(provide employee-in-team?)
-(define (employee-in-team? employee team)
+(provide employee-in-team?/id)
+(define (employee-in-team?/id employee team)
   (ormap (lambda (team-member)
            (equal? (hash-ref employee
                              'employee_id)
                    (car team-member)))
          team))
+
+(provide employee-in-team?/schedule)
+(define (employee-in-team?/id employee team)
+  (define (schedule-in-team? schedule)
+    (findf (lambda (team-schedule)
+             (equal? schedule team-schedule))
+           team))
+
+  (ormap schedule-in-team?
+         (hash-ref employee 'schedules)))
