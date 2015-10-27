09c4b2ad02cfb201f18d6be4372e69af749abea0
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-27 15:23:00 +0200

    Add slack message output and checking for new RoW vacations

---
+++ src/slack.rkt
@@ -0,0 +1,30 @@
+#lang racket/base
+
+(require racket/port
+         net/http-client
+         net/url
+         net/uri-codec
+         json
+
+         "slack-information.rkt")
+
+(define (slack-message msg #:host [host hook-host]
+                       #:url [url hook-url])
+  (define request-data
+    (jsexpr->string
+      `#hash((text . ,msg))))
+
+  (define-values (response headers input-port)
+    (http-sendrecv host
+                   url
+                   #:ssl? #t
+                   #:method "POST"
+                   #:data
+                   (alist->form-urlencoded
+                     (list (cons 'payload request-data)))
+                   #:headers
+                   (list "Content-Type: application/x-www-form-urlencoded")))
+  (port->string input-port))
+
+(module+ main
+  (slack-message "This is a message.\n"))
---
+++ src/vacation-check.rkt
@@ -0,0 +1,51 @@
+#lang racket/base
+
+(require "shiftplanning-api/vacations.rkt")
+
+(define seen-vacations (make-parameter '()))
+
+(define (add-seen-vacation v)
+  (seen-vacations (cons v
+                        (seen-vacations))))
+
+(define (write-seen-vacations vs
+                              [path "data/seen-vacations"])
+  (call-with-output-file
+    path
+    (lambda (out)
+      (write vs out))
+    #:exists 'replace))
+
+(define (read-seen-vacations [path "data/seen-vacations"])
+  (call-with-input-file
+    path
+    read))
+
+(define (seen-vacation? v)
+  (findf (lambda (seen-vacation)
+           (equal? (hash-ref seen-vacation
+                             'id)
+                   (hash-ref v
+                             'id)))
+         (seen-vacations)))
+
+(define (new-vacation? v)
+  (not (seen-vacation? v)))
+
+(define (new-vacations [team-name "cs_row"])
+  (filter new-vacation?
+          (get/vacations/team team-name)))
+
+(define (fetch-loop)
+  (sleep 240)
+  (seen-vacations (read-seen-vacations))
+  (define new (new-vacations))
+  (for-each add-seen-vacation new)
+  (for-each (lambda (v)
+              (slackmessage (format "~a has requested a vacation!")))
+            new)
+  (write-seen-vacations (seen-vacations)))
+
+(module+ main
+  (fetch-loop))
+
