d1297642a6f14a595996738189c4f9a359bfd13c
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-28 13:37:40 +0200

    Add month lookup table for integer representations

--- src/shiftplanning-api/shiftplanning-dates.rkt
+++ src/shiftplanning-api/shiftplanning-dates.rkt
@@ -2,6 +2,22 @@
 
 (require racket/date)
 
+(define (number->month n)
+  (define months
+    #hash((1 . "Jan")
+          (2 . "Feb")
+          (3 . "Mar")
+          (4 . "Apr")
+          (5 . "May")
+          (6 . "Jun")
+          (7 . "Jul")
+          (8 . "Aug")
+          (9 . "Sep")
+          (10 . "Oct")
+          (11 . "Nov")
+          (12 . "Dec")))
+  (hash-ref months n))
+
 (provide date->shiftplanning-date)
 (define (date->shiftplanning-date d)
   (format "~a ~a, ~a"
