95d77a7214561d914bb195c1522399ee286c7dc2
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-28 14:19:14 +0200

    Fix erroneous type sent in request

--- src/shiftplanning-api/shifts.rkt
+++ src/shiftplanning-api/shifts.rkt
@@ -16,14 +16,24 @@
               #:request-parameters
               `#hash((start_date . ,start-date)
                      (end_date . ,end-date)
-                     (type . "schedule")))
+                     (type . "schedule_summary")))
     (api-call #:module "reports.schedule"
               #:method "GET"
               #:request-parameters
               `#hash((start_date . ,start-date)
                      (end_date . ,end-date)
                      (employees . ,employees)
-                     (type . "schedule")))))
+                     (type . "schedule_summary")))))
+
+(define (start-of-month)
+  (date 0 0 0
+        1 (date-month (current-date)) (date-year (current-date))
+        0 0 #f 0))
+
+(define (end-of-month)
+  (date 0 0 0
+        31 (date-month (current-date)) (date-year (current-date))
+        0 0 #f 0))
 
 (define (get/on-now)
   (api-call #:module "dashboard.onnow"
@@ -38,8 +48,8 @@
 (module+ main
   (require racket/pretty)
   (define cd (current-date))
-  (define start-of-month (date 0 0 0 1 (date-month cd) (date-year cd) 0 0 #f 0))
-  (define end-of-month (date 0 0 0 31 (date-month cd) (date-year cd) 0 0 #f 0))
+  (define start (start-of-month))
+  (define end (end-of-month))
   (pretty-print
-    (get/report/shifts #:start (date->shiftplanning-date start-of-month)
-                       #:end (date->shiftplanning-date end-of-month))))
+    (get/report/shifts #:start (date->shiftplanning-date (start-of-month))
+                       #:end (date->shiftplanning-date (end-of-month)))))
